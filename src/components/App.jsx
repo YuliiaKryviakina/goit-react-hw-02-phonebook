b6c88ab5e43a341ba37639fe1e34ee8f982ccd2c
YuliiaKryviakina <krivyakina.yulya@gmail.com>
2023-07-15 02:34:19 +0300

    step2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
-import { nanoid } from "nanoid";
-import { ContactForm } from "./ContactForm/ContactForm";
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
 
 export class App extends Component {
   state = {
-  contacts: [],
-  name: ''
-}
+    contacts: [(module.id = nanoid())],
+    name: '',
+  };
 
   render() {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <h1>Phonebook</h1>
-      <ContactForm/>
-      
-    </div>
-  );
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm handleSubmit={this.handleSubmit} />
+        <h2>Contacts</h2>
+        <Filter filter={filter} handleChange={this.handleChange} />
+      </div>
+    );
+  }
 }
-  
-};
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,6 +6,7 @@ export class ContactForm extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   handleChange = e => {
@@ -21,10 +22,10 @@ export class ContactForm extends Component {
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { name, number } = this.state;
 
     return (
-      <form className={css.form}>
+      <form className={css.form} onSubmit={this.handleSubmit}>
         <label className={css.formLabel}>Name</label>
         <input
           className={css.formName}
@@ -33,6 +34,21 @@ export class ContactForm extends Component {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          placeholder="Enter name"
+          value={name}
+          onChange={this.handleChange}
+        />
+        <label className={css.formLabel}>Number</label>
+        <input
+          className={css.formNumber}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="Enter phone number"
+          value={number}
+          onChange={this.handleChange}
         />
         <button className={css.formBtn} type="submit">
           Add contact
